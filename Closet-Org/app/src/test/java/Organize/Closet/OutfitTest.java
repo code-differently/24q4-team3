9a902dde0667be17e09c4d95ac9f9effda598847
AmiyahJo <ajones244@gmail.com>
2024-11-06 21:14:49 +0000

    feat: adds OutfitTest

--- Closet-Org/app/src/test/java/Organize/Closet/OutfitTest.java
+++ Closet-Org/app/src/test/java/Organize/Closet/OutfitTest.java
@@ -1,5 +1,8 @@
 package Organize.Closet;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.junit.jupiter.api.Test;
 
 public class OutfitTest {
@@ -7,28 +10,68 @@ public class OutfitTest {
     @Test 
     public void testDescribeOutfit() {
         //Arrange 
+        Clothing shirt = new topItem("Blue");
+        Clothing pants = new bottomItem("Black");
+        Clothing accesory = new accesory("");
+
+        Outfit outfit = new Outfit();
+        outfit.addClothing(shirt);
+        outfit.addClothing(pants);
+        outfit.addClothing(accesory);
+
+
         //Act
+        String description = outfit.describe();
+
         //Assert
+        assertEquals();
     }
 
     @Test 
     public void testIsInCloset() {
-        //Arrange 
-        //Act
-        //Assert
+        // Arrange
+        Outfit outfit = new Outfit();
+        Closet closet = new Closet();
+        closet.addOutfit(outfit);
+
+        // Act
+        boolean exists = closet.contains(outfit);
+
+        // Assert
+        assertTrue(exists);
     }
 
     @Test 
     public void testGetSeasonalPurpose() {
-        //Arrange 
-        //Act
-        //Assert
+      // Arrange
+        Clothing coat = new Coat("Red", "L", "Winter");
+        Clothing scarf = new Scarf("Black", "Winter");
+
+        Outfit outfit = new Outfit();
+        outfit.addClothing(coat);
+        outfit.addClothing(scarf);
+
+        // Act
+        String season = outfit.getSeasonalPurpose();
+
+        // Assert
+        assertEquals("Winter", season);
     }
 
     @Test 
     public void testGetStyle() {
-        //Arrange 
-        //Act
-        //Assert
+        // Arrange
+        Clothing tshirt = new TShirt("Blue", "M", "Casual");
+        Clothing jeans = new Jeans("Black", "L", "Casual");
+
+        Outfit outfit = new Outfit();
+        outfit.addClothing(tshirt);
+        outfit.addClothing(jeans);
+
+        // Act
+        String style = outfit.getStyle();
+
+        // Assert
+        assertEquals("Casual", style);
     }
 }
