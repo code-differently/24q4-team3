33cc4ad371ce7558956a2e0b764ac364cc83e31e
AmiyahJo <ajones244@gmail.com>
2024-11-06 20:34:05 +0000

    feat: adds fullbody and outfit tests , removes corresponding classes/interface(s)
    abiding by the TDD steps , deciding to make the test FIRST rather than the actual code

---
+++ Closet-Org/app/src/test/java/Organize/Closet/FullbodyTest.java
@@ -0,0 +1,32 @@
+package Organize.Closet;
+
+import org.junit.jupiter.api.Test;
+
+public class FullbodyTest {
+
+    @Test
+    void testDescribeOutfit() {
+        
+    }
+
+    @Test
+    void testGetMaterial() {
+        
+    }
+
+    @Test
+    void testGetSleeveLength() {
+        
+    }
+
+    @Test
+    void testSetMaterial() {
+        
+    }
+
+    @Test
+    void testSetSleeveLength() {
+        
+    }
+    
+}
---
+++ Closet-Org/app/src/test/java/Organize/Closet/OutfitTest.java
@@ -0,0 +1,46 @@
+package Organize.Closet;
+
+public class OutfitTest {
+    
+    @Test 
+    public void testGetColor() {
+        //Arrange 
+        //Act
+        //Assert
+    }
+
+    @Test 
+    public void testDescribeOutfit() {
+        //Arrange 
+        //Act
+        //Assert
+    }
+
+    @Test 
+    public void testIsInCloset() {
+        //Arrange 
+        //Act
+        //Assert
+    }
+
+    @Test 
+    public void testGetSeasonalPurpose() {
+        //Arrange 
+        //Act
+        //Assert
+    }
+
+    @Test 
+    public void testGetStyle() {
+        //Arrange 
+        //Act
+        //Assert
+    }
+
+    @Test 
+    public void testGetColor() {
+        //Arrange 
+        //Act
+        //Assert
+    }
+}
