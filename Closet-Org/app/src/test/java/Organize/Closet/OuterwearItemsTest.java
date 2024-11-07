91e95ea698970b2d7f388269580fc166f709cc1a
Yafiaha <ynadbullah244@gmail.com>
2024-11-07 16:01:45 +0000

    Feat: outerwear test

--- Closet-Org/app/src/test/java/Organize/Closet/OuterwearItemsTest.java
+++ Closet-Org/app/src/test/java/Organize/Closet/OuterwearItemsTest.java
@@ -1,3 +1,16 @@
 package Organize.Closet;
 
-public class OuterwearItemsTest {}
+import org.junit.jupiter.api.BeforeEach;
+
+public class OuterwearItemsTest {
+ private OuterwearItems outerwear;
+
+  @BeforeEach
+  public void setUp() {
+    outerwear = new OuterwearItems("Red", "Casual", "Scarf", "Casual", "Winter", "Scarf");
+  }
+
+
+}
+
+
