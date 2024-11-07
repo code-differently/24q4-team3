c8fb7b93d87cd31d903c896d76d591f6c9e56db9
yafiaha <72883973+yafiaha@users.noreply.github.com>
2024-11-07 09:55:34 -0500

    Delete Closet-Org/app/src/main/java/Organize/Closet/OuterWear.java

--- Closet-Org/app/src/main/java/Organize/Closet/OuterWear.java
+++
@@ -1,6 +0,0 @@
-package Organize.Closet;
-
-public interface  OuterWear  extends ArticleOfClothing{
-    boolean isWaterproof();
-    String getInsulationLevel(); // e.g., "Light", "Medium", "Heavy"
-}
