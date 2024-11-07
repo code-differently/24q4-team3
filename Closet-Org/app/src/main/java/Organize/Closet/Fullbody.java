46189f57ec0be84b905dfc5487168e8338b18612
AmiyahJo <ajones244@gmail.com>
2024-11-07 21:23:16 +0000

    feat: adds fullbody item and interface

---
+++ Closet-Org/app/src/main/java/Organize/Closet/Fullbody.java
@@ -0,0 +1,9 @@
+package Organize.Closet;
+
+public interface Fullbody {
+    boolean hasPockets();
+    String getMaterial();
+    String getSleeveLength();
+    String getClosureType();
+    boolean isMachineWashable();    
+}
