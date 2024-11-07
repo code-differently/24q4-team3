4b65b6a1fcdd42736ed1d27b5bf894d05979f0c0
AmiyahJo <ajones244@gmail.com>
2024-11-07 18:19:55 +0000

    rm: Outfit.java

--- Closet-Org/app/src/main/java/Organize/Closet/Outfit.java
+++ Closet-Org/app/src/main/java/Organize/Closet/Outfit.java
@@ -1,24 +1,5 @@
 package Organize.Closet;
 
 public class Outfit {
-    private BottomItems pants;
-    // private Weather weather;  
 
-    public Outfit(BottomItems pants) {
-        this.pants = pants;
-        // this.weather = weather;
-    }
-
-    public BottomItems getPants() {
-        return pants;
-      }
-
-    public void setPants(BottomItems pants) {
-        this.pants = pants;
-      }
-
-      public String describeOutfit() {
-        return "The outfit for today is " + pants.getName();
-        // ", perfect for " + weather.getCondition() + ".";
-      }
 }
