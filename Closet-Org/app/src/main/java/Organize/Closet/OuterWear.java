2b2a1f4e829e84b5809ccf1e03afeddb765a7872
haldanek <khaldane244@gmail.com>
2024-11-08 20:25:19 +0000

    feat: readding files after merge conflict

---
+++ Closet-Org/app/src/main/java/Organize/Closet/OuterWear.java
@@ -0,0 +1,7 @@
+package Organize.Closet;
+
+public interface OuterWear extends ArticleOfClothing {
+  boolean isWaterproof();
+
+  String getInsulationLevel(); // e.g., "Light", "Medium", "Heavy"
+}
