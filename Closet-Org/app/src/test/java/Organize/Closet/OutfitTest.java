6e1fa139259adb5cb4592de44c318e65dbacb6a5
AmiyahJo <ajones244@gmail.com>
2024-11-08 19:12:28 +0000

    feat: adds outfit class & test , temperature exception along with a 'getName()' for fullbody.java

---
+++ Closet-Org/app/src/test/java/Organize/Closet/OutfitTest.java
@@ -0,0 +1,40 @@
+package Organize.Closet;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class OutfitTest {
+    private BottomItems jeans;
+    private Outfit outfit;
+
+    @BeforeEach
+    public void setUp() {    
+        jeans = new BottomItems("Jeans", "blue", "casual", "none", "autumn", "high-waist", true, "none");
+        outfit = new Outfit(jeans, null);
+    }
+
+    
+    @Test
+    void testGetPants() {
+        assertEquals("Jeans", outfit.getPants().getName());
+    }
+
+    @Test
+    void testSetPants() {
+       
+        BottomItems shorts = new BottomItems("Shorts", null, null, null, null, null, false, null);
+        outfit.setPants(shorts);
+
+        assertEquals("Shorts", outfit.getPants().getName());
+    }
+
+    @Test
+    void testDescribeOutfit() {
+       
+        String description = outfit.describeOutfit();
+        assertEquals("The outfit for today is Jeans", description);
+    }
+
+
+}
