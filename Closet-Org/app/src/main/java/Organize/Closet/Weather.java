8a5541ee061c8a0515d381ea3402f6f577bea304
Yafiaha <ynadbullah244@gmail.com>
2024-11-08 21:24:06 +0000

    fix-up

--- Closet-Org/app/src/main/java/Organize/Closet/Weather.java
+++ Closet-Org/app/src/main/java/Organize/Closet/Weather.java
@@ -1,15 +1,10 @@
 package Organize.Closet;
 
-<<<<<<< HEAD
 public interface Weather { 
     double getTemperature();
     String getCondition();
     int getHumidity();
     String getSeason(); 
-=======
-public interface Weather {
-    
->>>>>>> main
 }
    
 
