4cb880e78c0d1d84cdeb31bd8f750d79dcaddbe9
Mya <86239804+AmiyahJo@users.noreply.github.com>
2024-12-05 10:55:48 -0500

    feat: adds christmas scene (#49)
    Co-authored-by: AmiyahJo <ajones244@gmail.com>

---
+++ capstone/src/app/components/ChristmasScene.tsx
@@ -0,0 +1,9 @@
+// This file will pull the christmas tree and scroll together 
+const ChristmasScene = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+export default ChristmasScene;
