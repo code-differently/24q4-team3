37ce45a699a032b23104469d2fbf35bf182c0664
AmiyahJo <ajones244@gmail.com>
2024-12-05 20:18:46 +0000

    fix: merge conflicts

--- capstone/src/app/page.tsx
+++ capstone/src/app/page.tsx
@@ -1,19 +1,13 @@
-<<<<<<< HEAD
-import ChristmasScene from "./components/ChristmasScene";
-
-export default function Home() {
-  return (
-    <div>
-      <ChristmasScene/>
-=======
 
 import WishlistForm from './components/WishlistForm';
+import ChristmasScene from "./components/ChristmasScene";
 
 export default function Home() {
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-100 p-4">
       <WishlistForm />
->>>>>>> 3990610 (FEAT: Backend Integration API & Mongodb Database WishlistForm (#52))
+      <ChristmasScene/>
+      
     </div>
   );
 }
