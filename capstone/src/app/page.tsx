a458a695776e43559ac5ed3e41f2111b0ffea8be
yafiaha <72883973+yafiaha@users.noreply.github.com>
2024-12-05 14:22:47 -0500

    FEAT: Backend Integration API & Mongodb Database WishlistForm (#52)

--- capstone/src/app/page.tsx
+++ capstone/src/app/page.tsx
@@ -5,9 +5,6 @@ export default function Home() {
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-100 p-4">
       <WishlistForm />
-      <ChristmasScene/>
-      
     </div>
   );
 }
-
