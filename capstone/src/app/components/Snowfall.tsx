ef00df6c5071e5281e25b24215bf98768fbcf9f2
AmiyahJo <ajones244@gmail.com>
2024-12-09 18:44:39 +0000

    rm: comments in snowfall

--- capstone/src/app/components/Snowfall.tsx
+++ capstone/src/app/components/Snowfall.tsx
@@ -1,19 +1,18 @@
 "use client";
 import React, { useEffect } from "react";
-import "./Snowfall.css"; // Importing the CSS for the snowfall animation
+import "./Snowfall.css"; 
 
 const Snowfall = () => {
   useEffect(() => {
-    // Snowflake fall logic (optional, if you want to add JavaScript-based effects)
-    const numSnowflakes = 100; // Number of snowflakes
+    const numSnowflakes = 100; 
     const snowflakesContainer = document.querySelector(".snowflakes");
 
     for (let i = 0; i < numSnowflakes; i++) {
       const snowflake = document.createElement("div");
       snowflake.classList.add("snowflake");
-      snowflake.style.left = `${Math.random() * 100}vw`; // Random horizontal position
-      snowflake.style.animationDuration = `${Math.random() * 3 + 2}s`; // Random animation duration
-      snowflake.style.animationDelay = `${Math.random() * 5}s`; // Random animation delay
+      snowflake.style.left = `${Math.random() * 100}vw`; 
+      snowflake.style.animationDuration = `${Math.random() * 3 + 2}s`; 
+      snowflake.style.animationDelay = `${Math.random() * 5}s`; 
       snowflakesContainer?.appendChild(snowflake);
     }
 
