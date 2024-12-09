a608592ad632a5b429dee1f3ed5629bb0f196b32
Mya <86239804+AmiyahJo@users.noreply.github.com>
2024-12-09 14:26:30 -0500

    feat: adds christmas colors , snowfall animation , etc. (#62)
    * feat: adds design changes tp wishlist form
    
    * rm: green border from wishlist div
    
    * rm: scroll file
    
    no longer needed after wishlist form was created
    
    * fix: wishlistForm format
    
    feat: adds custom colors in tailwind, adjusts root back and foregrounds
    
    * feat: adds snowfall animation
    
    removes global css import in signup form <- not neccesary
    
    * rm: comments in snowfall
    
    ---------
    
    Co-authored-by: AmiyahJo <ajones244@gmail.com>

--- capstone/tailwind.config.ts
+++ capstone/tailwind.config.ts
@@ -13,6 +13,13 @@ export default {
         foreground: "var(--foreground)", 
         gold: "#D78628", 
         pink: "#FF69B4",
+        chestnut: "#59O411",
+        nutmeg: "#70271B",
+        icecream: "#F4C7BA",
+        green: "#513C17",
+        beige: "#AF8264",
+        brown: "#873C1C",
+        rose: "#EA8A71",
       },
       animation: {
         blink: "blink 1s infinite", 
