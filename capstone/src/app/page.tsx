b0170cf385cd49a0ed063a9e6ff0e999de9bf25c
AmiyahJo <ajones244@gmail.com>
2024-12-11 18:46:50 +0000

    fix: merge conflict

--- capstone/src/app/page.tsx
+++ capstone/src/app/page.tsx
@@ -1,24 +1,15 @@
-"use client";
 
-import Link from "next/link";
-import ChristmasScene from '../components/ChristmasScene';
-import WishlistForm from '../components/WishlistForm';
-import Snowfall from '../components/Snowfall';
-import Image from 'next/image'; 
+import ChristmasScene from './components/ChristmasScene';
+import WishlistForm from './components/WishlistForm';
+import Snowfall from './components/Snowfall';
 
 export default function Home() {
   return (
     <div className="flex flex-col md:flex-row items-center justify-center min-h-screen bg-nutmeg p-4 gap-8">
       <Snowfall /> 
       <WishlistForm />
-      <ChristmasScene />
-      
-      {/* Send Invitation Button */}
-      <Link href="/send-invitation">
-        <button className="mt-6 p-3 bg-yellow-500 text-white rounded hover:bg-blue-700">
-          Send Invitation
-        </button>
-      </Link>
+      <ChristmasScene/>
     </div>
   );
 }
+
