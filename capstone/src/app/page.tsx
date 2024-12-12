ccab54135dcdb2086486ff5dadadeb3d016211cb
AmiyahJo <ajones244@gmail.com>
2024-12-12 19:30:47 +0000

    feat: adds invite button
    routes to the send-invitation page for the user to access the invitation form

--- capstone/src/app/page.tsx
+++ capstone/src/app/page.tsx
@@ -2,13 +2,17 @@
 import ChristmasScene from '../components/ChristmasScene';
 import WishlistForm from '../components/WishlistForm';
 import Snowfall from '../components/Snowfall';
+import InviteButton from './components/InviteButton';
 
 export default function Home() {
   return (
     <div className="flex flex-col md:flex-row items-center justify-center min-h-screen bg-nutmeg p-4 gap-8">
       <Snowfall /> 
       <WishlistForm />
+      <div>
       <ChristmasScene/>
+      <InviteButton/>
+      </div>
     </div>
   );
 }
