a647516af5ce90af703a21ebe3832bae660deb2e
Yafiaha <ynadbullah244@gmail.com>
2024-12-12 14:06:12 +0000

    Fix: delete babel.config

--- capstone/babel.config.js
+++
@@ -1,8 +0,0 @@
-// babel.config.js
-module.exports = {
-    presets: [
-      '@babel/preset-env',  // For modern JavaScript features
-      '@babel/preset-react' // For JSX and React
-    ],
-  };
-  
